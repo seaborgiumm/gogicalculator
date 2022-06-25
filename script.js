6647144e5048e1383d460fccd5f2e5aab0c97b6f
seaborgiumm <70629621+seaborgiumm@users.noreply.github.com>
2022-06-26 01:52:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let deliveries = document.getElementById('deliveries').value;
 let km = document.getElementById('km').value;
 let sallary = 0;
     if(deliveries >= 1){
-        sallary = (total - deliveries*50)*0.15 + deliveries*50 + km*3.6
+        sallary = (total - deliveries*50)*0.15 + deliveries*50 + km*5
     }
     else{
-        sallary = total * 0.15 + km*3.6
+        sallary = total * 0.15 + km*5
     }
 let youGet = document.getElementById('youGet');
     youGet.innerText = (sallary + ' грн')
