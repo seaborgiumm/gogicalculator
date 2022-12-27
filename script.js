d7f8a7b9a2dd986bce179f907d185a3e99a39b1c
seaborgiumm <70629621+seaborgiumm@users.noreply.github.com>
2022-12-27 16:05:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let deliveries = document.getElementById('deliveries').value;
 let km = document.getElementById('km').value;
 let sallary = 0;
     if(deliveries >= 1){
-        sallary = (total - deliveries*50)*0.08 + 600 + deliveries*50 + km*5
+        sallary = (total - deliveries*60)*0.08 + 600 + deliveries*60 + km*5
     }
     else{
         sallary = 600 + (total * 0.08) + km*5
